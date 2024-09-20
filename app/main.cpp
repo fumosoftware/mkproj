35744dbd709ba36789a6d1abaca07c48bf139eb7
fumosoft <projectacorn@fumosoft.com>
2024-09-20 07:53:19 -0500

    Added basic cmakepresets

--- app/main.cpp
+++ app/main.cpp
@@ -3,8 +3,26 @@
 //
 
 #include <iostream>
+#include <expected>
+#include <string>
+
+using namespace std::string_literals;
+auto is_even(int i) -> std::expected<double, std::string> {
+  if(i % 2 == 0) return 1.0;
+
+  return std::unexpected{"This is not an even number"s};
+}
 
 int main(int argc, char** argv) {
-    std::cout << "Hello World!\n";
-    return 0;
+  std::cout << "Hello World!\n";
+
+  if(auto const num = is_even(2); num.has_value()) {
+    std::cout << "The number is " << num.value() << "\n";
+  }
+
+  if(auto const num = is_even(1); !num.has_value()) {
+    std::cout << num.error() << "\n";
+  }
+
+  return 0;
 }
