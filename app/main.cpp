b417965f4893051f3c231f9264908202d456311d
fumosoft <projectacorn@fumosoft.com>
2024-09-30 22:06:45 -0500

    added docopt

--- app/main.cpp
+++ app/main.cpp
@@ -3,7 +3,23 @@
 //
 
 #include <fmt/os.h>
+#include <docopt/docopt.h>
+
+namespace {
+  constexpr auto USAGE =
+R"(mkproj
+
+    Usage:
+      mkproj (new | create) <name>
+      mkproj (-h | --help)
+      mkproj (-v | --version)
+    Options:
+      -h --help         Show this screen.
+      -v --version      Show version.
+)";
+}
 
 int main(int argc, char** argv) {
+  auto args = docopt::docopt(USAGE, {argv + 1, argv + argc}, true, "mkproj version 1.0");
   fmt::print("Hello World!\n");
 }
