c1288342d2c78a99760cad0d78ef816b63849bf4
fumosoft <projectacorn@fumosoft.com>
2024-09-30 19:50:48 -0500

    Somehow got vcpkg working

--- app/main.cpp
+++ app/main.cpp
@@ -6,23 +6,8 @@
 #include <expected>
 #include <string>
 
-using namespace std::string_literals;
-auto is_even(int i) -> std::expected<double, std::string> {
-  if(i % 2 == 0) return 1.0;
-
-  return std::unexpected{"This is not an even number"s};
-}
+#include <fmt/os.h>
 
 int main(int argc, char** argv) {
-  std::cout << "Hello World!\n";
-
-  if(auto const num = is_even(2); num.has_value()) {
-    std::cout << "The number is " << num.value() << "\n";
-  }
-
-  if(auto const num = is_even(1); !num.has_value()) {
-    std::cout << num.error() << "\n";
-  }
-
-  return 0;
+  fmt::print("Hello WorQ!\n");
 }
