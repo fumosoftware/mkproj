40ed38e6920bfa4094895935a7759a752580ce07
fumosoft <projectacorn@fumosoft.com>
2024-09-30 21:48:02 -0500

    Remove unused headers

--- app/main.cpp
+++ app/main.cpp
@@ -2,12 +2,8 @@
 // Created by fumosoft on 9/19/2024.
 //
 
-#include <iostream>
-#include <expected>
-#include <string>
-
 #include <fmt/os.h>
 
 int main(int argc, char** argv) {
-  fmt::print("Hello WorQ!\n");
+  fmt::print("Hello World!\n");
 }
