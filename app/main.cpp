a2b245aa55b318a90a3ca6df2827a8442f642064
fumosoft <projectacorn@fumosoft.com>
2024-09-30 23:20:28 -0500

    added project name validation

--- app/main.cpp
+++ app/main.cpp
@@ -4,7 +4,10 @@
 
 #include <fmt/os.h>
 #include <docopt/docopt.h>
+#include <string>
+#include <expected>
 
+using namespace std::string_literals;
 namespace {
   constexpr auto USAGE =
 R"(mkproj
@@ -19,7 +22,43 @@ R"(mkproj
 )";
 }
 
-int main(int argc, char** argv) {
-  auto args = docopt::docopt(USAGE, {argv + 1, argv + argc}, true, "mkproj version 1.0");
-  fmt::print("Hello World!\n");
+std::expected<void, std::string> validate_project_name(std::string_view project_name) {
+  fmt::print("Validing project name.");
+  for(auto iter = project_name.begin(); iter != project_name.end(); iter = std::next(iter)) {
+    if(*iter == '.') {
+      if(*std::next(iter) == '.') {
+        return std::unexpected("Project name must not contain `..`"s);
+      }
+    }
+
+    if(*iter == '\\') {
+      return std::unexpected(R"(Project name must not contain `\`)"s);
+    }
+  }
+
+  return {};
+}
+
+[[nodiscard]]std::expected<void, std::string> create_new_proj(std::string_view project_name) {
+  if(project_name.length() > 20) return std::unexpected("Project name too long"s);
+
+  if(auto const maybe = validate_project_name(project_name); !maybe.has_value()) return maybe;
+
+  return {};
+}
+
+int main(int const argc, char** argv) {
+  auto const args = docopt::docopt(USAGE,
+    {argv + 1, argv + argc},
+    true,
+    "mkproj version 1.0");
+
+  if(args.contains("new") || args.contains("create")) {
+    auto const project_name = args.at("<name>").asString();
+    if(auto const maybe = create_new_proj(project_name); !maybe.has_value()) {
+      fmt::print("Failed to create new project {}. Reason: {}\n", project_name, maybe.error());
+    }
+  }
+
+  return 0;
 }
